9f2a404721f21f7abf8072453c2e38c0004a7312
Andrew Stam <andrew.cstam@gmail.com>
2019-06-15 16:44:32 -0400

    Add updateUser fn, update starter data

--- src/main/java/com/example/myapp/controllers/UserController.java
+++ src/main/java/com/example/myapp/controllers/UserController.java
@@ -14,9 +14,9 @@ import java.util.Arrays;
 public class UserController {
     // Test Data of Users
     private User[] users = {
-            new User(1L, "andrew", "stam", "Andrew", "Stam", "22 October 1980", RoleType.GroupLeader, "example@example.com"),
-            new User(2L, "bob123", "pass", "Bob", "Smith", "25 February 1995", RoleType.GroupMember, "example1@example.com"),
-            new User(3L, "mymember", "mypass", "Member", "LastName", "31 July 2001", RoleType.GroupLeader, "example2@example.com")
+            new User(1L, "andrew", "stam", "Andrew", "Stam", "1980-10-22", RoleType.GroupLeader, "example@example.com"),
+            new User(2L, "bob123", "pass", "Bob", "Smith", "1995-02-25", RoleType.GroupMember, "example1@example.com"),
+            new User(3L, "mymember", "mypass", "Member", "LastName", "2001-07-31", RoleType.GroupLeader, "example2@example.com")
     };
 
     private User invalid = new User(null, null, null, RoleType.GroupMember);
@@ -36,7 +36,7 @@ public class UserController {
             }
         }
         // Didn't find
-        return null;
+        return invalid;
     }
 
     // Delete a User by their ID
@@ -64,6 +64,21 @@ public class UserController {
     public User[] updateUsers(@RequestBody User[] allUsers) {
         users = allUsers;
         return users;
+    }
+
+    // Update given user, return updated user
+    @PostMapping("/api/users/{uid}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public User updateUser(@PathVariable("uid") Long id, @RequestBody User update) {
+        for (int i = 0; i < users.length; i++) {
+            User u = users[i];
+            if (u.getId().equals(id)) {
+                users[i] = update;
+                return update;
+            }
+        }
+        // Didn't update
+        return null;
     }
 
     // Create a new user, return if success
