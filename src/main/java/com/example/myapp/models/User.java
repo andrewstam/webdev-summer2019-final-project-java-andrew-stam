c913f0516a08cdacbe5e66460b8e5761b5449bc6
Andrew Stam <andrew.cstam@gmail.com>
2019-06-18 15:40:12 -0400

    ID is no longer generated with SQL, instead is recieved from frontend

--- src/main/java/com/example/myapp/models/User.java
+++ src/main/java/com/example/myapp/models/User.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
