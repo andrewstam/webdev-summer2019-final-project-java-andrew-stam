2beab3b8f0e6b3801b12bebc324720fd8e375e9f
Andrew Stam <andrew.cstam@gmail.com>
2019-06-20 17:26:03 -0400

    Generate getters and setters

--- src/main/java/com/example/myapp/models/Group.java
+++ src/main/java/com/example/myapp/models/Group.java
@@ -17,4 +17,50 @@ public class Group {
     private List<Long> memberIds;
 
     private List<WatchItem> watchItems;
+
+    public Group(Long groupId, Long leaderId, List<Long> memberIds, List<WatchItem> watchItems) {
+        this.groupId = groupId;
+        this.leaderId = leaderId;
+        this.memberIds = memberIds;
+        this.watchItems = watchItems;
+    }
+
+    public Group() {
+        this.groupId = null;
+        this.leaderId = null;
+        this.memberIds = null;
+        this.watchItems = null;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public Long getLeaderId() {
+        return leaderId;
+    }
+
+    public void setLeaderId(Long leaderId) {
+        this.leaderId = leaderId;
+    }
+
+    public List<Long> getMemberIds() {
+        return memberIds;
+    }
+
+    public void setMemberIds(List<Long> memberIds) {
+        this.memberIds = memberIds;
+    }
+
+    public List<WatchItem> getWatchItems() {
+        return watchItems;
+    }
+
+    public void setWatchItems(List<WatchItem> watchItems) {
+        this.watchItems = watchItems;
+    }
 }
--- src/main/java/com/example/myapp/models/WatchItem.java
+++ src/main/java/com/example/myapp/models/WatchItem.java
@@ -2,7 +2,6 @@
 package com.example.myapp.models;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -14,9 +13,65 @@ public class WatchItem {
 
     private String movieId;
 
-    private Date watchDate;
+    private String watchDate;
 
     private List<Comment> comments;
 
     private List<Long> attendingMemberIds;
+
+    public WatchItem(Long itemId, String movieId, Strin gwatchDate, List<Comment> comments, List<Long> attendingMemberIds) {
+        this.itemId = itemId;
+        this.movieId = movieId;
+        this.watchDate = watchDate;
+        this.comments = comments;
+        this.attendingMemberIds = attendingMemberIds;
+    }
+
+    public WatchItem() {
+        this.itemId = null;
+        this.movieId = null;
+        this.watchDate = null;
+        this.comments = null;
+        this.attendingMemberIds = null;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getWatchDate() {
+        return watchDate;
+    }
+
+    public void setWatchDate(String watchDate) {
+        this.watchDate = watchDate;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<Long> getAttendingMemberIds() {
+        return attendingMemberIds;
+    }
+
+    public void setAttendingMemberIds(List<Long> attendingMemberIds) {
+        this.attendingMemberIds = attendingMemberIds;
+    }
 }
