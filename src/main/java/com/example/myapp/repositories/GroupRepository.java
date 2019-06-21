50c9b1448114074aff77769c86bd672db84f38f6
Andrew Stam <andrew.cstam@gmail.com>
2019-06-20 23:27:38 -0400

    More repository methods, add/del users to groups, add/del watch items

--- src/main/java/com/example/myapp/repositories/GroupRepository.java
+++ src/main/java/com/example/myapp/repositories/GroupRepository.java
@@ -29,9 +29,39 @@ public interface GroupRepository extends CrudRepository<Group, Long> {
     @Query(value = "select user_id from user_groups where groups_id=:id", nativeQuery = true)
     public List<Long> findGroupMemberIds(@Param("id") Long id);
 
+    // Use JPA to add a user to a group
+    @Modifying
+    @Transactional
+    @Query(value = "insert into user_groups (group_id, user_id) values (:gid, :uid)", nativeQuery = true)
+    public void addUser(@Param("gid") Long gid, @Param("uid") Long uid);
+
+    // Use JPA to remove a user from a group
+    @Modifying
+    @Transactional
+    @Query(value = "delete from user_groups where group_id=:gid and user_id=:uid", nativeQuery = true)
+    public void deleteUser(@Param("gid") Long gid, @Param("uid") Long uid);
+
     // Use JPA to find the group's WatchItems
     @Query(value = "select * from watchitem where group_id=:id", nativeQuery = true)
     public List<WatchItem> findGroupWatchItems(@Param("id") Long id);
+
+    // Use JPA to add a watch item
+    @Modifying
+    @Transactional
+    @Query(value = "insert into watchitem (group_id, movie_id, watch_date) values (:gid, :mid, :dte)", nativeQuery = true)
+    public void addWatchItem(@Param("gid") Long gid, @Param("mid") String mid, @Param("dte") String date);
+
+    // Use JPA to remove a watch item
+    @Modifying
+    @Transactional
+    @Query(value = "delete from watchitem where group_id=:gid and movie_id=:mid and watch_date=:dte", nativeQuery = true)
+    public void deleteWatchItem(@Param("gid") Long gid, @Param("mid") String mid, @Param("dte") String date);
+
+    // Use JPA to update a watch item's date
+    @Modifying
+    @Transactional
+    @Query(value = "update watchitem set watch_date=:dte where group_id=:gid and movie_id=:mid", nativeQuery = true)
+    public void updateWatchItemDate(@Param("gid") Long gid, @Param("mid") String mid, @Param("dte") String date);
 
     // Use JPA to find the group's Movie Ids
     @Query(value = "select movie_id from watchitem where group_id=:id", nativeQuery = true)
@@ -84,29 +114,4 @@ public interface GroupRepository extends CrudRepository<Group, Long> {
     // Use JPA to find a groups's watch item's users who wrote comments by its id, return that list
     @Query(value = "select user_id from comment where watch_item_id=:wid", nativeQuery = true)
     public List<Long> findItemUser(@Param("wid") Long wid);
-
-    /*
-    // Use JPA to add a group of a movie by a user by their ids, storing the text and star rating
-    @Modifying
-    @Transactional
-    @Query(value = "insert into group (user_id, movie_id, group_text, stars) values (:uid, :mid, :txt, :stz)", nativeQuery = true)
-    public void createGroup(@Param("uid") Long uid, @Param("mid") String mid, @Param("txt") String text, @Param("stz") Integer stars);
-
-    // Use JPA to update a star rating of a movie by a user by their ids
-    @Modifying
-    @Transactional
-    @Query(value = "update group set stars=:stz where user_id=:uid and movie_id=:mid", nativeQuery = true)
-    public void updateStars(@Param("uid") Long uid, @Param("mid") String mid, @Param("stz") Integer stars);
-
-    // Use JPA to update group text of a movie by a user by their ids
-    @Modifying
-    @Transactional
-    @Query(value = "update group set group_text=:txt where user_id=:uid and movie_id=:mid", nativeQuery = true)
-    public void updateText(@Param("uid") Long uid, @Param("mid") String mid, @Param("txt") String text);
-
-    // Use JPA to delete a group of a movie by a user by their ids
-    @Modifying
-    @Transactional
-    @Query(value = "delete from group where user_id=:uid and mid=:mid", nativeQuery = true)
-    public void deleteGroup(@Param("uid") Long uid, @Param("mid") String mid);*/
 }
